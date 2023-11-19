5dcb8431a14ac8dd53553affd11bff977a964180
Jakub Lisowski <lisowski.jakub@gmail.com>
2023-11-19 21:52:21 +0100

    Task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { Profile } from './Profile/Profile';
+import user  from './Profile/user'
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    style={{
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
     >
-      React homework template
+      <Profile username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}>
+      </Profile>
     </div>
   );
 };
